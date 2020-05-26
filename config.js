6d8213a476d8503c8e95ce0c8fbfdf655a4c903e
Jaewook Lee <ljw5073@gmail.com>
2020-05-26 00:49:58 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
-export const googleApiKey = 'AIzaSyBoX_m8IQZO2Fq__4XgkIRADhVXTJwtsbs';
-export const googleSheetDocId = '1AKmXQRHbW2Cu0VpVzM5wewL5W8HtQRgTwhdAugdqJJU';
+export const googleApiKey = 'AIzaSyBiXQ0cbZlWwpUjLkl8evPdCBFh34JO1DM';
+export const googleSheetDocId = '1nL8TFhob4WJoMx7Hd7Evw-A3BbwwgIpfwcINb9rwVj8';
